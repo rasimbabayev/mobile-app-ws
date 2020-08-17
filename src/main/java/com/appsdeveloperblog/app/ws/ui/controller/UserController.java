01126decfa3e6004916d995df816efed77f74f6b
Rasim Babayev <42684444+rasimbabayev@users.noreply.github.com>
2020-08-17 16:02:09 +0400

    first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+
+    public String getUser() {
+        return "get user was called";
+    }
+}
